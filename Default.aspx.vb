abb0cfc4d834f611cdfc8142fa2ec824bb7b5b1d
Sam Hoeger <sam-hoeger@uiowa.edu>
2014-05-11 13:58:46 -0500

    Modified code for submit button

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -45,15 +45,51 @@ Partial Class _Default
     End Sub
 
 
-
-
-
-
-
-
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Label6.Text = TextBox1.Text
+
+        Label2.Visible = False
+        TextBox1.Visible = False
+        TextBox2.Visible = False
+        Label7.Visible = False
+        femaleRadioButton.Visible = False
+        maleRadioButton.Visible = False
+        Label10.Visible = False
+        Calendar1.Visible = False
+        Label14.Visible = False
+        Button1.Visible = False
+
+        Label3.Visible = True
+        Label4.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        Label8.Visible = True
+        Label11.Visible = True
+        Label13.Visible = True
+        Label15.Visible = True
+        Label9.Visible = True
+        Label12.Visible = True
+        HyperLink1.Visible = True
+
+        Label9.Text = Calendar1.SelectedDate
+
+
+        If femaleRadioButton.Checked = True Then
+            Label5.Visible = True
+            Label4.Text = ""
+        Else
+            Label4.Visible = True
+            Label5.Text = ""
+        End If
+
+        Dim money As Decimal = Val(TextBox2.Text)
+        Label12.Text = String.Format("{0:C}", money)
 
 
 
     End Sub
+
+
+
 End Class
